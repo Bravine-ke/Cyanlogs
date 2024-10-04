544a729be4656eec2b40295babac4f186af36f35
Brav Kauzi <j17-3036-2019@student.mksu.ac.ke>
2024-10-04 13:00:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
+import os
+
 class Config:
-    SECRET_KEY = 'your_secret_key'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
+    GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
+    GOOGLE_DISCOVERY_URL = os.environ.get('GOOGLE_DISCOVERY_URL')
